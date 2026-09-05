33ca342056b98f2705a16eaf70df889345fff0be
J-Tabosa <jtabosa028@gmail.com>
2026-09-04 23:15:51 -0300

    chore: atualiza cache offline para v3

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE = 'agenda-financeira-v2';
-const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.json','./icons/icon.svg'];
+const CACHE = 'agenda-financeira-v3';
+const ASSETS = ['./','./index.html','./styles.css','./app.js','./refine-v3.css','./refine-v3.js','./manifest.json','./icons/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
     const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response;
-  }).catch(() => caches.match('./index.html'))));
+  }).catch(() => event.request.mode === 'navigate' ? caches.match('./index.html') : undefined)));
 });
