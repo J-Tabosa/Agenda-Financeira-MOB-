b0bfd52159148bcdc032cb3e2c1297536fe20cb6
J-Tabosa <jtabosa028@gmail.com>
2026-09-05 00:48:14 -0300

    chore: atualiza cache para motor financeiro v5

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='agenda-financeira-v4';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icons/icon.svg'];
+const CACHE = 'agenda-financeira-v5';
+const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.json','./icons/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>{const network=fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>cached||caches.match('./index.html'));return cached||network}))});
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>event.request.mode==='navigate'?caches.match('./index.html'):undefined)))});
